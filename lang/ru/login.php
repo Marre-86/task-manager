222ac77c73001fcae496d00cf89713e14a42313e
Artem Pokhiliuk <timtimoha@yandex.ru>
2023-03-29 23:21:05 +0600

    Made russian localization for register/login forms

---
+++ lang/ru/login.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'email' => 'Email',
+    'password' => 'Пароль',
+    'remember_me' => 'Запомнить меня',
+    'forgot_your_password' => 'Забыли пароль?',
+    'log_in' => 'Войти',
+
+
+];
---
+++ lang/ru/register.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'name' => 'Имя',
+    'email' => 'Email',
+    'password' => 'Пароль',
+    'confirm_password' => 'Подтверждение',
+    'already_registered' => 'Уже зарегистрированы?',
+    'register' => 'Зарегистрировать',
+
+
+];
