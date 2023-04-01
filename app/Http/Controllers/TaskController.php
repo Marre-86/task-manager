a0b2e8a4f6389abc7c25985b1190c6b590fc8ae1
Artem Pokhiliuk <timtimoha@yandex.ru>
2023-04-01 18:03:47 +0600

    Trying to fix hexlet autotest notices

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -105,7 +105,7 @@ class TaskController extends Controller
         $data = $validator->validated();
         $task = new Task();
         $task->fill($data);
-        $task->created_by_id = Auth::id() ?? 99999;
+        $task->created_by_id = intval(Auth::id()) ?? 99999;   // because of PHPStan so cumbersome
         $task->save();
 
         if (($request->input('labels')) !== null) {
